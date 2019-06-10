95c6136ebb1fcfdc7497b11de233d17f89a1affb
jiaxh <jia_xiaohong@xiniaoyun.com>
2019-06-10 19:00:07 +0800

    update

---
+++ src/main/java/com/jiaxh/security/demo/code/DemoImageCodeGenerator.java
@@ -0,0 +1,20 @@
+package com.jiaxh.security.demo.code;
+
+import com.jiaxh.security.core.validate.code.ImageCode;
+import com.jiaxh.security.core.validate.code.ValidateCodeGenerator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.ServletWebRequest;
+
+/**
+ * @Auther: jiaxh
+ * @Date: 2019/6/10 14:55
+ */
+//@Component("imageCodeGenerator")
+//public class DemoImageCodeGenerator implements ValidateCodeGenerator {
+//
+//    @Override
+//    public ImageCode createImageCode(ServletWebRequest request) {
+//        System.out.println("更高级的图形验证码生成器");
+//        return null;
+//    }
+//}
